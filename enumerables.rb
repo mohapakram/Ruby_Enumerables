16d6f644c0999c0c1b34b37c0cf6a47f7937b82f
mohab akram <mohapmobile@gmail.com>
2020-08-18 15:19:07 +0200

    Added linter config

--- enumerables.rb
+++ enumerables.rb
@@ -1,114 +1,111 @@
 module Enumerable
-    def my_each
-      return to_enum(:my_each) unless block_given?
-      each do |item|
-        yield item
-      end
-
-      self
+  def my_each
+    return to_enum(:my_each) unless block_given?
+    each do |item|
+      yield item
     end
 
-    def my_each_with_index 
-      return to_enum(:my_each_with_index ) unless block_given?
+    self
+  end
 
-      index = 0
-      each do |item|
-        yield item, index
-        index += 1
-      end
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
 
-      self
+    index = 0
+    each do |item|
+      yield item, index
+      index += 1
     end
 
-   def my_select 
-    return to_enum(:my_select ) unless block_given?
+    self
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
 
     results_array = []
     each do |item|
-     next unless yield item
-     results_array << item 
+      next unless yield item
+      results_array << item
     end
     results_array
-   end
+  end
 
-
-   def my_all?(pattern = nil)
-     result = true
-     if block_given?
-        each do |item|
-        result = false unless yield item
-        end 
-     elsif pattern 
-        each do |item|
-         result = false unless pattern.match(item)
-        end 
-     else 
-        each do |item|
-        result = false unless item
-        end 
-     end
-    result
-   end 
-
-   def my_none?(pattern = nil)
+  def my_all?(pattern = nil)
     result = true
     if block_given?
-      each do |item| 
+      each do |item|
+        result = false unless yield item
+      end
+    elsif pattern
+      each do |item|
+        result = false unless pattern.match(item)
+      end
+    else
+      each do |item|
+        result = false unless item
+      end
+    end
+    result
+  end
+
+  def my_none?(pattern = nil)
+    result = true
+    if block_given?
+      each do |item|
         result = false if yield item
-      end 
-   elsif pattern 
+      end
+    elsif pattern
       each do |item|
-       result = false if pattern.match(item)
-      end 
-   else 
+        result = false if pattern.match(item)
+      end
+    else
       each do |item|
-      result = false if item
-      end 
-   end
-  result
- end
-     
+        result = false if item
+      end
+    end
+    result
+  end
+
   def my_count(n = nil)
     count = 0
     if n
-        each do |i|
+      each do |i|
         count += n if i == n
-        end
-    end  
+      end
+    end
 
-    return length unless block_given? 
+    return length unless block_given?
 
     each do |i|
-        count += 1 if yield i
-    end 
+      count += 1 if yield i
+    end
     count
   end
 
-  def my_map(proc = nil) 
+  def my_map(proc = nil)
     end_array = []
+    item = nil
     each do |i|
-      if proc 
-       item = proc.call(i)
-       end_array << item 
-      else 
+      if proc
+        item = proc.call(i)
+      else
         return to_enum(:my_map) unless block_given?
         item = yield i
-        end_array << item 
       end
+      end_array.push item
     end
-   end_array
+    end_array
   end
 
-   def my_inject(acc = 0,&operation)
-   each do |i|
-    acc = operation.call(i,acc) 
-   end
-   acc
+  def my_inject(acc = 0)
+    each do |i|
+      acc = yield(i, acc)
+    end
+    acc
   end
-
 end
-      
 
 def multiply_els(array)
-  array.my_inject(1,&:*)
+  array.my_inject(1, &:*)
 end
