2306256aa50f238192309e0fd88fa12bcf59c8d3
mohab akram <mohapmobile@gmail.com>
2020-08-18 13:04:30 +0200

    my_each, my_each_with_index, my_select, my_all?

---
+++ enumerables.rb
@@ -0,0 +1,61 @@
+module Enumerable
+    def my_each
+      return to_enum(:my_each) unless block_given?
+      each do |item|
+        yield item
+      end
+
+      self
+    end
+
+    def my_each_with_index 
+      return to_enum(:my_each_with_index ) unless block_given?
+
+      index = 0
+      each do |item|
+        yield item, index
+        index += 1
+      end
+
+      self
+    end
+
+   def my_select 
+    return to_enum(:my_select ) unless block_given?
+
+    results_array = []
+    each do |item|
+     next unless yield item
+     results_array << item 
+    end
+    results_array
+   end
+
+
+   def my_all?(pattern = nil)
+     result = true
+     if block_given?
+        each do |item|
+        result = false unless yield item
+        end 
+     elsif pattern 
+        each do |item|
+         result = false unless pattern.match(item)
+        end 
+     else 
+        each do |item|
+        result = false unless item
+        end 
+     end
+    result
+   end 
+
+end
+
+
+array = [1,2,4]
+
+
+
+result = %w[ant bear cat].my_all?
+p result
