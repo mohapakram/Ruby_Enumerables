7a1dad4d62ff45507685350e0cab3e325925742e
mohab akram <mohapmobile@gmail.com>
2020-09-02 10:22:42 +0200

    Added test cases for #my_each_with_index

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -12,6 +12,16 @@ describe Enumerable do
         expect(num_array.my_each { |item| item*2 }).to eql(num_array)
     end
  end
+
+  describe "#my_each_with_index" do 
+    it "If no block is given, an enumerator is returned instead." do 
+      expect(num_array.my_each).to be_an Enumerator
+    end
+
+    it "Calls block with two arguments, the item and its index, for each item in enum." do
+        expect(num_array.my_each { |item, index| item*2 }).to eql(num_array)
+    end
+ end
 end
 
 
