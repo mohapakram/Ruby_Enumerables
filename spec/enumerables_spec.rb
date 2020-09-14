63fdf03d89fc4ad396b2b2ca39285fb6eb2052ed
GraceOyiza <graceoyiza13@gmail.com>
2020-09-14 15:25:08 +0100

    Add my_each method

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -3,7 +3,13 @@ require_relative "../enumerables.rb"
 describe "Enumerable" do
     let (:array) { [1, 2, 3, 4] }
     let (:answer) { [] }
-
+  
+    describe "#my_each" do
+    it "iterates through an array" do
+        array.my_each { |item| answer << item } 
+        expect(answer).to eq([1,2,3,4])
+    end    
+ end
   
 
  end
